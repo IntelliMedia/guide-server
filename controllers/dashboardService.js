2950287302737622aea68690fe401b06f9487330
Robert Taylor <rgtaylor@ncsu.edu>
2018-04-10 12:06:19 -0400

    Fixed #109 by calling Firebase DB set in parallel with returning a hint.

--- controllers/dashboardService.js
+++ controllers/dashboardService.js
@@ -41,14 +41,18 @@ class DashboardService {
 
           //console.info("Push this to firebase:\n" + JSON.stringify(data, null, 2));
           
-          session.debugAlert("Set student data in dashboard db; path=" + pathToUserITSData);      
-          return firebaseDb.ref(pathToUserITSData).set(data)
+          session.debugAlert("Set student data in dashboard db; path=" + pathToUserITSData);  
+          // Call this promise instead of returning it to run this in parallel since the Firebase DB
+          // is taking 60+ seconds for set to return.
+          firebaseDb.ref(pathToUserITSData).set(data)
             .then(() => {
               session.debugAlert("Successfully set student data in dashboard db.");
             })
             .catch((err) => {
               session.errorAlert("Unable to set student data in dashboard db: " + err);
             });
+
+          return Promise.resolve();
 
         } catch(err) {
           console.error("Failed to write this to dashboard:\n"+ JSON.stringify(data, null, 2));
