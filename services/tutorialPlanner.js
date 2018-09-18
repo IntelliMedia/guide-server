e0f67b8458a146c0f64cf0ff5232c4bf3c561e1e
Robert Taylor <rgtaylor@ncsu.edu>
2018-09-18 13:17:59 -0400

    As part of #195, added logging of tutor actions and their priorities to help debug why remediation actions aren't bubbling to the top.

--- services/tutorialPlanner.js
+++ services/tutorialPlanner.js
@@ -2,6 +2,7 @@
 
 const HintRecommender = require('./hintRecommender');
 const RemediationRecommender = require('./remediationRecommender');
+const stringx = require("../utilities/stringx");
 
 /**
  * This class decides whether or not to issue a hint based on the student model.
@@ -15,7 +16,7 @@ class TutorialPlanner {
     // Returns action or null if the the tutor shouldn't take an action
     evaluateAsync(event) {
 
-        // Future: add other possible tutorial actions (other than hinting) like 
+        // Future: add other possible tutorial actions (other than hinting) like
         // outer loop problem selection recommendation.
         let actionPromises = [];
 
@@ -32,14 +33,37 @@ class TutorialPlanner {
 
             // Combined actions from each promise into a single list
             results.forEach((result) =>  {
-                if (result && result.length > 0) { 
-                    actions = actions.concat(result); 
+                if (result && result.length > 0) {
+                    actions = actions.concat(result);
                 }
             });
 
             let filteredAndSorted = actions
                 .filter((action) => action != null)
                 .sort((a, b) => b.context.priority - a.context.priority);
+
+            this.session.debugAlert("Possible Tutor Actions: " + filteredAndSorted.length + "\n" + filteredAndSorted.map(a => {
+                let actionDescription;
+                if (a.action === "HINT") {
+                    actionDescription = "{0} | {1} | {2} | {3} | {4}".format(
+                        a.context.priority,
+                        a.action,
+                        a.context.conceptId,
+                        a.context.hintLevel,
+                        a.context.hintDialog);
+                } else if (a.action === "REMEDIATE") {
+                    actionDescription = "{0} | {1} | {2} | {3}".format(
+                        a.context.priority,
+                        a.action,
+                        a.context.conceptId,
+                        a.context.practiceCriteria);
+                } else {
+                    actionDescription =  "Unknown action: {0}".format(a.action);
+                }
+
+                return actionDescription;
+            }).join("\n"));
+
             return filteredAndSorted.length > 0 ? filteredAndSorted[0] : null;
         });
     }
